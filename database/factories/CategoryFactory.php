b89b10d8ec6006d6513e4d38941ee13ac5e64405
Buendy <danielbuendiasmr@gmail.com>
2019-02-05 16:09:48 +0000

    Se empiezan con los seeders

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->randomElements(
+            [
+                'PHP', 'JAVASCRIPT', 'JAVA', 'DISEÑO WEB', 'SERVIDORES', 'VUE', 'MYSQL',
+                'AWS', 'BIGDATA', 'DIGITAL OCEAN'
+            ]),
+        'description' => $faker->sentence
     ];
 });
