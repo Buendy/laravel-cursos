fcdad973772cc2c5fbd4c0154915ca48235edc66
Buendy <danielbuendiasmr@gmail.com>
2019-02-15 17:58:42 +0000

    Diseñado jumbotron del curso

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -5,4 +5,13 @@ return [
         'more_information' => 'More information',
         'jumbotron_title' => 'Acces inmeditatly'
     ],
+    'courses' => [
+        'course_name' => 'Course name',
+        'teacher_name' => 'Teacher name',
+        'category_name' => 'Category name',
+        'publish' => 'Date publish',
+        'updated' => 'date update',
+        'students' => 'Estudents',
+        'reviews' => 'Reviews'
+    ],
 ];
--- resources/lang/es/app.php
+++ resources/lang/es/app.php
@@ -5,4 +5,13 @@ return [
      'more_information' => 'Más información',
      'jumbotron_title' => 'Accede a cualquier curso de inmediato'
  ],
+    'courses' => [
+        'course_name' => 'Nombre del curso',
+        'teacher_name' => 'Nombre del profesor',
+        'category_name' => 'Nombre de la categoria',
+        'publish' => 'Fecha de publicación',
+        'updated' => 'Fecha de actualización',
+        'students' => 'Estudiantes',
+        'reviews' => 'Criticas'
+    ],
 ];
